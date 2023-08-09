a2db98d0556988ff6a92d209b5496f3436ad923d
Lolster24Vito <vitomir.hardi@gmail.com>
2023-08-09 21:54:18 +0200

    Soap working from tutorial

---
+++ src/main/java/hr/algebra/iis_soap/IisSoapApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.iis_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IisSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IisSoapApplication.class, args);
+	}
+
+}
